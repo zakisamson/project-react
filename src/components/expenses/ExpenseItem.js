9746b0580abacbc73e021a69d3f302113ddd0cb2
Zaki Samson <zakisamson@gmail.com>
2022-05-09 20:07:48 +0700

    introducing state to component

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
+import {useState} from 'react'
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  const [testing, setTesting] = useState(props.title)
+  const clickHandler = () => {
+    setTesting('testing')
+  }
   return (
-    
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{testing}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button type='button' onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
