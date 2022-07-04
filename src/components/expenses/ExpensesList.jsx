b7feb48bd64c4d5c9213759eae4821c64ee2efe8
Zaki Samson <zakisamson@gmail.com>
2022-07-04 15:50:23 +0700

    removed console log

--- src/components/expenses/ExpensesList.jsx
+++ src/components/expenses/ExpensesList.jsx
@@ -3,7 +3,6 @@ import ExpenseItem from './ExpenseItem'
 import './ExpensesList.css'
 
 const ExpensesList = (props) => {
-    console.log(props.items)
     if (props.items.length === 0) {
         return <h2 className='expenses-list__fallback'>Found no expenses.</h2>
     }
