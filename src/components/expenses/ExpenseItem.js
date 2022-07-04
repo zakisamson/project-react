a60ea77d19e33423f53d554bca15b8c30dc50b4d
Zaki Samson <zakisamson@gmail.com>
2022-07-04 14:30:14 +0700

    removing a state for exercise

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -4,18 +4,13 @@ import {useState} from 'react'
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const [testing, setTesting] = useState(props.title)
-  const clickHandler = () => {
-    setTesting('testing')
-  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{testing}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button type='button' onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
