4d1b914ca290692356ff152076ef7d86bfd2b9dd
Zaki Samson <zakisamson@gmail.com>
2022-07-04 14:08:47 +0700

    added new component

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,32 +1,44 @@
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import { useState } from 'react';
 
 function Expenses(props) {
+
+  const [expensesYearFilter, setExpensesYearFilter] = useState("")
+
+  const handleChangeYear = (year) => {
+    setExpensesYearFilter(year)
+    console.log(expensesYearFilter)
+  }
+
   return (
-    
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    <div>
+      <ExpensesFilter handleChangeYear={handleChangeYear}/>
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
