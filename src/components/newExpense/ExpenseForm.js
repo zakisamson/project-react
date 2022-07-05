0da3216bd3b9ef16131ef89703f67d4fca27f060
Zaki Samson <zakisamson@gmail.com>
2022-07-05 14:14:35 +0700

    my solution to hiding and showing form on click

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -10,6 +10,7 @@ function ExpenseForm(props) {
     amount: "",
     date: "",
   });
+  const [showDisplay, setShowDisplay] = useState(false)
 
   const titleChangeHandler = (event) => {
     // setUserInput({
@@ -56,43 +57,51 @@ function ExpenseForm(props) {
       amount: "",
       date: "",
     });
+
+    setShowDisplay(!showDisplay)
   };
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={userInput.title}
-            onChange={titleChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={userInput.amount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2050-12-31"
-            value={userInput.date}
-            onChange={dateChangeHandler}
-          />
-        </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add expense</button>
-        </div>
+    <div>
+      <div className="new-expense__actions" style={{display: showDisplay ? "none" : "block" }}>
+        <button onClick={() => setShowDisplay(!showDisplay)}>Add New Expense</button>
       </div>
-    </form>
+      <form onSubmit={submitHandler} style={{ display: showDisplay ? "block" : "none" }}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={userInput.title}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={userInput.amount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2050-12-31"
+              value={userInput.date}
+              onChange={dateChangeHandler}
+            />
+          </div>
+          <div className="new-expense__actions">
+            <button type="reset" onClick={() => setShowDisplay(!showDisplay)}>Cancel</button>
+            <button type="submit">Add expense</button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
